1795bbc3270f7fce10db2abe9caf9e2d169fa136
AviTS <avisabath@gmail.com>
2022-06-04 17:43:24 -0400

    added db and googlebookapihelper stuff

--- models/book.js
+++ models/book.js
@@ -2,6 +2,7 @@ const { ExpressError, BadRequestError } = require('../helpers/expressError');
 const db = require('../db');
 
 class Book {
+  //adds a book to DB if no duplicate. doesn't return anything.
   static async addBook({
     external_book_id,
     book_title,
@@ -16,15 +17,33 @@ class Book {
       [book_title]
     );
 
-    if (duplicateCheck.rows[0])
-      throw new BadRequestError(`Duplicate book: ${book_title}`, 400);
+    if (!duplicateCheck.rows[0]) {
+      const result = await db.query(
+        `INSERT INTO books (external_book_id, title, author, book_description, genre) VALUES ($1, $2, $3, $4, $5)`,
+        [
+          external_book_id,
+          book_title,
+          book_author,
+          book_description,
+          book_genre,
+        ]
+      );
+    }
+    return;
+    // if (duplicateCheck.rows[0])
+    //   throw new BadRequestError(`Duplicate book: ${book_title}`, 400);
 
+    // return result.rows[0];
+  }
+
+  static async getBook(external_book_id) {
     const result = await db.query(
-      `INSERT INTO books (external_book_id, title, author, book_description, genre) VALUES ($1, $2, $3, $4, $5)`,
-      [external_book_id, book_title, book_author, book_description, book_genre]
+      `SELECT * FROM books WHERE (external_book_id = $1)`,
+      [external_book_id]
     );
 
-    return result.rows[0];
+    const book = result.rows[0];
+    return book;
   }
 }
 
